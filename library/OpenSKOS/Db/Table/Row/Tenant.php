99239b2857513ec27b327b1f701fecd1c50482ad
markl <markl@7e7b83ad-d38b-431b-876c-ac3faf902dbd>
2011-10-14 13:34:33 +0000

    API for retrieval of Institutions
    git-svn-id: https://code.pictura-dp.nl/svn/openskos/trunk@24 7e7b83ad-d38b-431b-876c-ac3faf902dbd

--- library/OpenSKOS/Db/Table/Row/Tenant.php
+++ library/OpenSKOS/Db/Table/Row/Tenant.php
@@ -11,6 +11,7 @@ class OpenSKOS_Db_Table_Row_Tenant extends Zend_Db_Table_Row
 			$form = new Zend_Form();
 			$form
 				->addElement('text', 'name', array('label' => 'Name', 'required' => true))
+				->addElement('text', 'organisationUnit', array('label' => 'Organisation unit'))
 				->addElement('text', 'website', array('label' => 'Website'))
 				->addElement('text', 'email', array('label' => 'E-mail'))
 				->addElement('text', 'streetAddress', array('label' => 'Street Address'))
@@ -26,5 +27,79 @@ class OpenSKOS_Db_Table_Row_Tenant extends Zend_Db_Table_Row
 			$form->setDefaults($this->toArray());
 		}
 		return $form;
+	}
+	
+	public function getConceptsPerCollection()
+	{
+		$solr = OpenSKOS_Solr::getInstance();
+		$q = 'class:Concept tenant:'.$this->code;
+		$result = $solr->search($q, array(
+			'rows' => 0,
+			'facet' => 'true',
+			'facet.field' => 'collection'
+		));
+		$classes = array();
+		return $result['facet_counts']['facet_fields']['collection'];
+	}
+	
+	/**
+	 * @return DOMDocument;
+	 */
+	public static function getRdfDocument()
+	{
+		$doc = new DOMDocument();
+		$doc->appendChild($doc->createElement('rdf:RDF'));
+		$doc->documentElement->setAttribute('xmlns:rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+		$doc->documentElement->setAttribute('xmlns:v', 'http://www.w3.org/2006/vcard/ns#');
+		
+		return $doc;
+	}
+	
+	public function toRdf()
+	{
+		$helper = new Zend_View_Helper_ServerUrl();
+		$about = $helper->serverUrl('/api/institution/'.$this->code);
+		$data = array();
+		foreach ($this as $key => $val) {
+			$data[$key] = htmlspecialchars($val);
+		}
+		
+		$doc = self::getRdfDocument();
+		$VCard = $doc->documentElement->appendChild($doc->createElement('v:Vcard'));
+		$VCard->setAttribute('rdf:about', $about);
+		$VCard->appendChild($doc->createElement('v:fn', $data['name']));
+		
+		if ($this->website) {
+			$VCard->setAttribute('v:url', $this->website);
+		}
+		$node  = $VCard->appendChild($doc->createElement('rdf:Description'));
+		$node->appendChild($doc->createElement('v:organisation-name', $data['name']));
+		if ($this->organisationUnit) {
+			$node->appendChild($doc->createElement('v:organisation-unit', $data['organisationUnit']));
+		}
+		
+		if ($this->email) {
+			$VCard->appendChild($doc->createElement('v:email'))
+				->setAttribute('rdf:about', 'mailto:'.$this->email);
+		}
+		
+		
+		$adr = $doc->createElement('v:adr');
+		foreach (array('street-address', 'locality', 'postal-code', 'country-name') as $name) {
+			$dbName = preg_replace_callback(
+				'/\-([a-z])/', create_function(
+	            '$matches',
+	            'return strtoupper($matches[1]);'
+        	), $name);
+			if ($this->$dbName) {
+				$adr->appendChild($doc->createElement('v:'.$name, $data[$dbName]));
+			}
+		}
+		if ($adr->childNodes->length) {
+			$VCard->appendChild($adr);
+		}
+		
+		
+		return $doc;
 	}
 }
