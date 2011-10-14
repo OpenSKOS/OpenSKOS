dd214f7a9306ba5049641ac7be3ea8df540f171e
markl <markl@7e7b83ad-d38b-431b-876c-ac3faf902dbd>
2011-10-14 15:08:27 +0000

    CRUD for User management
    git-svn-id: https://code.pictura-dp.nl/svn/openskos/trunk@25 7e7b83ad-d38b-431b-876c-ac3faf902dbd

--- library/OpenSKOS/Controller/Plugin/Auth.php
+++ library/OpenSKOS/Controller/Plugin/Auth.php
@@ -7,12 +7,23 @@ class OpenSKOS_Controller_Plugin_Auth extends Zend_Controller_Plugin_Abstract
 	public function preDispatch(Zend_Controller_Request_Abstract $request)
 	{
 		if (!in_array($request->getModuleName(), $this->modules)) return;
-        $authInstance = Zend_Auth::getInstance();
+
+		$authInstance = Zend_Auth::getInstance();
         $resource = $request->getControllerName();
         $actionName = $request->getActionName();
         if ($authInstance->hasIdentity()) {
+        	if($authInstance->getIdentity()->active != 'Y') {
+				Zend_Auth::getInstance()->clearIdentity();
+				Zend_Session::forgetMe();
+				Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger')->setNamespace('error')->addMessage('Your account is blocked.');
+        		Zend_Controller_Action_HelperBroker::getStaticHelper('redirector')->direct('index', 'index', 'website');
+        	} elseif (!OpenSKOS_Db_Table_Users::isDashboardAllowed($authInstance->getIdentity()->type)) {
+				Zend_Auth::getInstance()->clearIdentity();
+				Zend_Session::forgetMe();
+				Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger')->setNamespace('error')->addMessage('Your account is not allowed to use the dasboard.');
+        		Zend_Controller_Action_HelperBroker::getStaticHelper('redirector')->direct('index', 'index', 'website');
+        	}
         } else {
-        	$role = 'gast';
         	if ($request->getControllerName()!='login') {
 	        	$request->setModuleName('dashboard')
 		              ->setControllerName('login')
---
+++ library/OpenSKOS/Db/Table/Users.php
@@ -0,0 +1,68 @@
+<?php
+
+class OpenSKOS_Db_Table_Users extends Zend_Db_Table 
+{
+	const USER_TYPE_DASHBOARD = 'dashboard';
+	const USER_TYPE_API = 'api';
+	const USER_TYPE_BOTH = 'both';
+	
+	static $types = array(
+		self::USER_TYPE_DASHBOARD,
+		self::USER_TYPE_API,
+		self::USER_TYPE_BOTH
+	);
+	
+	protected $_name = 'user';
+	
+	/**
+	 * Classname for row
+	 *
+	 * @var string
+	 */
+	protected $_rowClass = 'OpenSKOS_Db_Table_Row_User';
+	
+	protected $_referenceMap = array (
+		'Tenant' => array (
+			'columns' => 'tenant', 
+			'refTableClass' => 'OpenSKOS_Db_Table_Tenants', 
+			'refColumns' => 'code'
+		)
+	);
+	
+	public static function isDashboardAllowed($usertype)
+	{
+		return $usertype == self::USER_TYPE_BOTH || $usertype == self::USER_TYPE_DASHBOARD;
+	}
+	
+	public static function isApiAllowed($usertype)
+	{
+		return $usertype == self::USER_TYPE_BOTH || $usertype == self::USER_TYPE_API;
+	}
+	
+	protected function _uniqueFieldValue($fieldname, $value, $data)
+	{
+		//fetch the tenant:
+		$tenant = OpenSKOS_Db_Table_Tenants::fromIdentity();
+		if (null === $tenant) {
+			throw new Zend_Db_Table_Exception('This method needs a valid tenant from the Zend_Auth object');
+		}
+		$select = $this->select()
+			->where('tenant=?', $tenant->code)
+			->where($fieldname . '=?', $value);
+		if (isset($data['id'])) {
+			$select->where('NOT(id=?)', $data['id']);
+		}
+		return count($this->fetchAll($select)) === 0;
+	}
+	
+	public function uniqueEmail($email, Array $data)
+	{
+		return $this->_uniqueFieldValue('email', $email, $data);
+	}
+	
+	public function uniqueApiKey($apikey, Array $data)
+	{
+		return $this->_uniqueFieldValue('apikey', $apikey, $data);
+	}
+	
+}
