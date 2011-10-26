6d51b751e6fae886c2fbc2a6c15764bb687dae0e
markl <markl@7e7b83ad-d38b-431b-876c-ac3faf902dbd>
2011-10-26 09:26:14 +0000

    New translations, plus a special Strings class for all strings that are not easily detected in the code, but must be translated.
    git-svn-id: https://code.pictura-dp.nl/svn/openskos/trunk@45 7e7b83ad-d38b-431b-876c-ac3faf902dbd

---
+++ library/OpenSKOS/Strings.php
@@ -0,0 +1,30 @@
+<?php
+//extra strings for PO/MO generation:
+class OpenSKOS_Strings
+{
+	protected static function translatables()
+	{
+		throw new Exception('This is just for Gettext PO/MO editors');
+		_('dashboard');
+		_('api');
+		_('both');
+		_('Supplied credential is invalid.');
+		_('Authentication successful.');
+		_("Invalid type given, value should be a string");
+		_("'%value%' is no valid email address in the basic format local-part@hostname");
+		_("'%hostname%' is no valid hostname for email address '%value%'");
+		_("'%hostname%' does not appear to have a valid MX record for the email address '%value%'");
+		_("'%hostname%' is not in a routable network segment. The email address '%value%' should not be resolved from public network.");
+		_("'%localPart%' can not be matched against dot-atom format");
+		_("'%localPart%' can not be matched against quoted-string format");
+		_("'%localPart%' is no valid local part for email address '%value%'");
+		_("'%value%' exceeds the allowed length");
+		_("Invalid type given, value should be a string");
+		_("'%value%' is less than %min% characters long");
+		_("'%value%' is more than %max% characters long");
+		_("Value is required and can't be empty");
+		_("The token '%token%' does not match the given token '%value%'");
+        _('No token was provided to match against');
+        _("The two given tokens do not match");
+	}
+}
