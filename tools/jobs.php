550cba1cf51db456ed2cfb47bf0dc06680c1c066
markl <markl@7e7b83ad-d38b-431b-876c-ac3faf902dbd>
2011-10-27 15:08:51 +0000

    job code
    git-svn-id: https://code.pictura-dp.nl/svn/openskos/trunk@49 7e7b83ad-d38b-431b-876c-ac3faf902dbd

--- tools/jobs.php
+++ tools/jobs.php
@@ -25,7 +25,7 @@ $actions = array('list', 'delete', 'process');
 $args = $OPTS->getRemainingArgs();
 if (!$args || count($args)!=1) {
 	echo str_replace('[ options ]', '[ options ] action', $OPTS->getUsageMessage());
-	fwrite(STDERR, "Expected an actions (".implode('|', $actions).")\n");
+	fwrite(STDERR, "Expected an action (".implode('|', $actions).")\n");
 	exit(1);
 }
 $action = $args[0];
@@ -42,7 +42,7 @@ switch ($action) {
 	case 'list':
 		$db = Zend_Db_Table::getDefaultAdapter();
 		$select = $db->select()
-			->from('job', array('id', 'collection', 'created', 'parameters'))
+			->from('job', array('id', 'collection', 'created', 'parameters', 'task'))
 			->join('collection', 'collection.id=job.collection', array('tenant', 'collectioncode' => 'code'))
 			->where('finished IS NULL')
 			->order('created asc')
@@ -51,22 +51,26 @@ switch ($action) {
 			$select->where('collection.tenant=?', $OPTS->tenant);
 		}
 		$rows = $db->fetchAll($select);
-		$columns = array('id', 'tenant', 'collection', 'created', 'file', 'size');
+		$columns = array('id', 'tenant', 'collection', 'created         ', 'task');
 		echo fwrite(STDOUT, implode("\t", $columns)."\n");
 		foreach ($rows as $row) {
 			$params = OpenSKOS_Db_Table_Jobs::getParams($row['parameters']);
 			fwrite(STDOUT, "{$row['id']}\t");
 			fwrite(STDOUT, "{$row['tenant']}\t");
-			fwrite(STDOUT, "{$row['collectioncode']}\t");
+			fwrite(STDOUT, str_pad($row['collectioncode'], strlen('collection'), ' ', STR_PAD_RIGHT). "\t");
 			fwrite(STDOUT, "{$row['created']}\t");
-			fwrite(STDOUT, "{$params['name']}\t");
-			fwrite(STDOUT, "{$params['size']}\n");
+			fwrite(STDOUT, "{$row['task']}\t");
+			if ($row['task'] === OpenSKOS_Db_Table_Row_Job::JOB_TASK_IMPORT) {
+				fwrite(STDOUT, "{$params['name']}\t");
+				fwrite(STDOUT, "{$params['size']}");
+			}
+			fwrite(STDOUT, "\n");
 		}
 		break;
 	case 'delete':
 	case 'process':
 		if ($OPTS->tenant) {
-			fwrite(STDERR, "`tenant` option not allowed with process action\n");
+			fwrite(STDERR, "`tenant` option not allowed with delete and process actions\n");
 			exit(1);
 		}
 		$model = new OpenSKOS_Db_Table_Jobs();
@@ -77,7 +81,10 @@ switch ($action) {
 				exit(1);
 			}
 		} else {
-			$jobs = $model->fetchAll();
+			$jobs = $model->fetchAll($model->select()
+			->where('finished IS NULL')
+			->order('created asc')
+			->where('started IS NULL'));
 		}
 		if (!count($jobs)) {
 			exit(0);
@@ -101,30 +108,58 @@ switch ($action) {
 				exit(0);
 			}
 			foreach ($jobs as $job) {
-				$file = $job->getFile();
-				if($job->isZip($file)) {
-					$zip = new ZipArchive();
-					if ($zip->open($file)!==true) {
-						fwrite(STDERR, "cannot open <$file>\n");
-						$job
-							->error('cannot open ZIP')
-							->finish()->save();
+				$collection = $job->getCollection();
+				switch ($job->task) {
+					case OpenSKOS_Db_Table_Row_Job::JOB_TASK_IMPORT:
+						$job->start();
+						$file = $job->getFile();
+						if($job->isZip($file)) {
+							$zip = new ZipArchive();
+							if ($zip->open($file)!==true) {
+								fwrite(STDERR, "cannot open <$file>\n");
+								$job
+									->error('cannot open ZIP')
+									->finish()->save();
+								break;
+							}
+							
+							if ($zip->numFiles >= 1) {
+								$msg = _('only ZIP files with exactly one file can be processed');
+								fwrite(STDERR, "{$job->id}/{$file}: {$msg}\n");
+								$job
+									->error($msg)
+									->finish()->save();
+								break;
+							}
+							for ($i=0; $i<$zip->numFiles;$i++) {
+						    	echo "index: $i\n";
+		    					print_r($zip->statIndex($i));
+							}
+							print_r($zip);
+						}
 						break;
-					}
-					
-					if ($zip->numFiles >= 1) {
-						$msg = _('only ZIP files with exactly one file can be processed');
-						fwrite(STDERR, "{$file}: {$msg}\n");
-						$job
-							->error($msg)
-							->finish()->save();
+					case OpenSKOS_Db_Table_Row_Job::JOB_TASK_HARVEST:
+						$job->start();
+						$harvester = OpenSKOS_Oai_Pmh_Harvester::factory($collection)
+							->setFrom($job->getParam('from'))
+							->setUntil($job->getParam('until'))
+							->setOption('set', $job->getParam('set'));
+						try {
+							foreach ($harvester as $page => $records) {
+								echo "page ".($page + 1).":\n";
+								foreach ($records as $r => $record) {
+									echo "  record ".($r+1).": {$record->identifier}\n";
+								}
+							}
+							$job->finish()->save();
+						} catch (OpenSKOS_Oai_Pmh_Harvester_Exception $e) {
+							fwrite(STDERR, $job->id.': '.$e->getMessage()."\n");
+							$job->error($e->getMessage())->finish()->save();
+						}
 						break;
-					}
-					for ($i=0; $i<$zip->numFiles;$i++) {
-				    	echo "index: $i\n";
-    					print_r($zip->statIndex($i));
-					}
-					print_r($zip);
+					default:
+						fwrite(STDERR, '@TODO: write handler for task='.$job->task."\n");
+						break;
 				}
 			}
 		}
