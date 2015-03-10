d463f575d098c8a2cad2c95cb093c0022faaa817
Alexandar Mitsev <a.mitsev@bluebuffstudio.com>
2015-03-10 08:57:50 +0200

    Add the longly missing openskos xsd schema. refs #20172

--- data/dbchanges/20150309.sql
+++ data/dbchanges/20150309.sql
@@ -2,4 +2,4 @@ ALTER TABLE `collection`
     ADD COLUMN `enableStatusesSystem` BOOLEAN;
 
 REPLACE INTO `namespace`
-    VALUES ('openskos', 'http://openskos.org/xmlns#');
+    VALUES ('openskos', 'http://openskos.org/xmlns/openskos.xsd');
