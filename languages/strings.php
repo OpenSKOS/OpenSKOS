a1f5aaeb65ee3154ed6bbc251b1d648bf904be4d
David Thornley <d.thornley@picturae.nl>
2015-05-07 09:33:09 +0200

    Translate all not translated strings.

---
+++ languages/strings.php
@@ -0,0 +1,10 @@
+<?php
+// Used to translate strings not wrapped in translate functions.
+
+_('candidate');
+_('approved');
+_('redirected');
+_('not_compliant');
+_('rejected');
+_('obsolete');
+_('deleted');
