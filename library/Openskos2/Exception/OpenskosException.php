090a1f651020f5d98242e60fbf5e0fb85ee118e2
jsmit <j.smit@picturae.nl>
2015-08-26 15:46:25 +0200

    first version of new library

---
+++ library/Openskos2/Exception/OpenskosException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenSkos2\Exception;
+
+
+/**
+ * Created by PhpStorm.
+ * User: jsmit
+ * Date: 26/08/2015
+ * Time: 15:35
+ */
+class OpenskosException extends \Exception
+{
+
+}
---
+++ library/Openskos2/Exception/ResourceNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OpenSkos2\Exception;
+
+/**
+ * Created by PhpStorm.
+ * User: jsmit
+ * Date: 26/08/2015
+ * Time: 14:38
+ */
+class ResourceNotFoundException extends OpenskosException
+{
+
+}
