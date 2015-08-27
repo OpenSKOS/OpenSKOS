5b1ce149ae04003cdbe2fd7ac22c224dfe84d9b6
amitsev <amitsev@lin3860770ffe72.corp.picturae.com>
2015-08-27 11:54:31 +0200

    Add PHP Di. Configure easy rdf lib. refs #22742

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OpenSKOS
  *
@@ -18,31 +19,29 @@
  * @author     Mark Lindeman
  * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
  */
-
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     protected function _initDefaultTimeZone()
     {
         date_default_timezone_set('UTC');
     }
-	protected function _initRestRoute()
-	{
-		$this->bootstrap('frontController');	
-		$front = $this->getResource('FrontController');
-		$restRoute = new Zend_Rest_Route(
-			$front, 
-			array(), 
-			array(
-				'api'
-			)
-		);
-		$front->getRouter()->addRoute('rest', $restRoute);
-	}
-    
+
+    protected function _initRestRoute()
+    {
+        $this->bootstrap('frontController');
+        $front = $this->getResource('FrontController');
+        $restRoute = new Zend_Rest_Route(
+            $front,
+            array(),
+            array('api')
+        );
+        $front->getRouter()->addRoute('rest', $restRoute);
+    }
+
     protected function _initXmlnsRoute()
-	{
-		$this->bootstrap('frontController');
-		$this->getResource('FrontController')->getRouter()->addRoute(
+    {
+        $this->bootstrap('frontController');
+        $this->getResource('FrontController')->getRouter()->addRoute(
             'xmlns',
             new Zend_Controller_Router_Route(
                 'xmlns',
@@ -53,17 +52,40 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
                 ]
             )
         );
-	}
-	
-	public function _initActionHelpers()
-	{
-    	// register the OpenSKOS action helpers
-		Zend_Controller_Action_HelperBroker::addPath(APPLICATION_PATH . '/../library/OpenSKOS/Controller/Action/Helper', 'OpenSKOS_Controller_Action_Helper');
-	}
-	
-	public function _initAuth()
-	{
-//	    Zend_Controller_Front::getInstance()->registerPlugin(new Application_Plugin_Auth($acl));
-	}
-}
+    }
 
+    public function _initActionHelpers()
+    {
+        // register the OpenSKOS action helpers
+        Zend_Controller_Action_HelperBroker::addPath(
+            APPLICATION_PATH . '/../library/OpenSKOS/Controller/Action/Helper',
+            'OpenSKOS_Controller_Action_Helper'
+        );
+    }
+
+    public function _initAuth()
+    {
+        //Zend_Controller_Front::getInstance()->registerPlugin(new Application_Plugin_Auth($acl));
+    }
+    
+    protected function _initContainer()
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->addDefinitions(APPLICATION_PATH . '/configs/di.config.php');
+        
+        //!TODO Configure the caching
+//        $cache = new MemcachedCache();
+//        $memcached = new Memcached();
+//        $memcached->addServer('localhost', 11211);
+//        $cache->setMemcached($memcached);
+//        $cache->setNamespace('OpenSkos');
+//        $builder->setDefinitionCache($cache);
+        
+        $container = $builder->build();
+
+        $dispatcher = new \DI\Bridge\ZendFramework1\Dispatcher();
+        $dispatcher->setContainer($container);
+
+        Zend_Controller_Front::getInstance()->setDispatcher($dispatcher);
+    }
+}
---
+++ tools/temp/test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * OpenSKOS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GPLv3 license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   OpenSKOS
+ * @package    OpenSKOS
+ * @copyright  Copyright (c) 2015 Pictura Database Publishing. (http://www.pictura-dp.nl)
+ * @author     Alexandar Mitsev
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
+
+
+include dirname(__FILE__) . '/../autoload.inc.php';
+
+/* 
+ * Updates the status expired to status obsolete
+ */
+
+require_once 'Zend/Console/Getopt.php';
+$opts = array(
+    'env|e=s' => 'The environment to use (defaults to "production")',
+);
+
+try {
+    $OPTS = new Zend_Console_Getopt($opts);
+} catch (Zend_Console_Getopt_Exception $e) {
+    fwrite(STDERR, $e->getMessage()."\n");
+    echo str_replace('[ options ]', '[ options ] action', $OPTS->getUsageMessage());
+    exit(1);
+}
+
+include dirname(__FILE__) . '/../bootstrap.inc.php';
+
+// Allow loading of application module classes.
+$autoloader = new OpenSKOS_Autoloader();
+$mainAutoloader = Zend_Loader_Autoloader::getInstance();
+$mainAutoloader->pushAutoloader($autoloader, array('Editor_', 'Api_'));
+
+
+// Test....
+
+/* @var $diContainer DI\Container */
+$diContainer = Zend_Controller_Front::getInstance()->getDispatcher()->getContainer();
+
+var_dump($diContainer->get('OpenSkos2\Rdf\ResourceManager'));
+
+var_dump($diContainer->make('OpenSkos2\Rdf\ResourceManager'));
+
+var_dump($diContainer->get('OpenSkos2\Rdf\ResourceManager'));
+
+
+// Inside the controllers use this
+//   /**
+//      * This dependency will be injected by PHP-DI
+//      * @Inject
+//      * @var Application_Service_GuestbookService
+//      */
+//    private $guestbookService;
