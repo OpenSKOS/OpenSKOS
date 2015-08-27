ba7df8c9a89cadb6fe2e420a3bb62596d814a728
jsmit <j.smit@picturae.nl>
2015-08-27 08:14:16 +0200

    Added special resources, Collections and managers

---
+++ library/Openskos2/ConceptManager.php
@@ -0,0 +1,15 @@
+<?php
+namespace OpenSkos2;
+
+use OpenSkos2\Rdf\ResourceManager;
+
+/**
+ * Created by PhpStorm.
+ * User: jsmit
+ * Date: 26/08/2015
+ * Time: 15:55
+ */
+class ConceptManager extends ResourceManager
+{
+
+}
