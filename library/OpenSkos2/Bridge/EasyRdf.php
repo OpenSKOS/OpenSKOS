5ee56fb6fd6e8ca13c76faf3312dd69f0c0b6efc
jsmit <j.smit@picturae.nl>
2015-08-27 16:38:48 +0200

    Added bridge between easy_rdf and our own rdf

---
+++ library/OpenSkos2/Bridge/EasyRdf.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OpenSkos2\Bridge;
+
+use EasyRdf_Graph;
+use EasyRdf_Literal;
+use EasyRdf_Resource;
+use OpenSkos2\Rdf\Literal;
+use OpenSkos2\Rdf\Resource;
+use OpenSkos2\Rdf\ResourceCollection;
+use OpenSkos2\Rdf\Uri;
+
+/**
+ * Created by PhpStorm.
+ * User: jsmit
+ * Date: 27/08/2015
+ * Time: 15:59
+ */
+class EasyRdf
+{
+    /**
+     * @param EasyRdf_Graph $graph
+     * @return ResourceCollection
+     */
+    public static function graphToResourceCollection(EasyRdf_Graph $graph)
+    {
+        $myResources = new ResourceCollection();
+        foreach ($graph->resources() as $resource) {
+            /** @var $resource EasyRdf_Resource */
+            $type = $resource->get('rdf:type');
+
+            if (!$type) {
+                continue;
+            }
+
+            $myResource = new Resource($resource->getUri());
+
+            foreach ($resource->propertyUris() as $propertyUri) {
+
+                foreach ($resource->all(new EasyRdf_Resource($propertyUri)) as $propertyValue) {
+                    if ($propertyValue instanceof EasyRdf_Literal) {
+                        $myResource->addProperty($propertyUri,
+                            new Literal($propertyValue->getValue(), $propertyValue->getLang()));
+                    } elseif ($propertyValue instanceof EasyRdf_Resource) {
+                        $myResource->addProperty($propertyUri, new Uri($propertyValue->getUri()));
+                    }
+                }
+            }
+
+            $myResources [] = $myResource;
+        }
+        return $myResources;
+    }
+
+    /**
+     * @param Resource $resource
+     * @return EasyRdf_Graph
+     */
+    public static function resourceToGraph(Resource $resource)
+    {
+        $easyResource = new \EasyRdf_Resource($resource->getUri(), new \EasyRdf_Graph());
+        foreach ($resource->getProperties() as $propName => $property) {
+            foreach ($property as $value) {
+                /**
+                 * @var $value Object
+                 */
+                if ($value instanceof Literal) {
+                    $easyResource->addLiteral($propName, $value->getValue(), $value->getLanguage());
+                } else {
+                    $easyResource->addResource($propName, $value->getValue());
+                }
+            }
+        }
+
+        $graph = $easyResource->getGraph();
+
+        return $graph;
+    }
+}
