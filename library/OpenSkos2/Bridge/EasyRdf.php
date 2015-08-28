f0b992a7ca41a00c5dd5003cf35f70a9fdeaac11
jsmit <j.smit@picturae.nl>
2015-08-28 11:11:18 +0200

    Updated licence information

--- library/OpenSkos2/Bridge/EasyRdf.php
+++ library/OpenSkos2/Bridge/EasyRdf.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * OpenSKOS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GPLv3 license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @category   OpenSKOS
+ * @package    OpenSKOS
+ * @copyright  Copyright (c) 2015 Picturae (http://www.picturae.com)
+ * @author     Picturae
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
+
 namespace OpenSkos2\Bridge;
 
 use EasyRdf_Graph;
@@ -13,12 +30,6 @@ use OpenSkos2\Rdf\ResourceCollection;
 use OpenSkos2\Rdf\Uri;
 use OpenSkos2\Schema;
 
-/**
- * Created by PhpStorm.
- * User: jsmit
- * Date: 27/08/2015
- * Time: 15:59
- */
 class EasyRdf
 {
     /**
--- library/OpenSkos2/ConceptCollection.php
+++ library/OpenSkos2/ConceptCollection.php
@@ -1,15 +1,26 @@
 <?php
+
+/**
+ * OpenSKOS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GPLv3 license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @category   OpenSKOS
+ * @package    OpenSKOS
+ * @copyright  Copyright (c) 2015 Picturae (http://www.picturae.com)
+ * @author     Picturae
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
 namespace OpenSkos2;
 
 use OpenSkos2\Rdf\Resource;
 use OpenSkos2\Rdf\ResourceCollection;
 
-/**
- * Created by PhpStorm.
- * User: jsmit
- * Date: 26/08/2015
- * Time: 15:54
- */
 class ConceptCollection extends ResourceCollection
 {
 
