ad328f939e37041b419dc6bf2ad54f35d78f36c4
olhsha <olha.chat@gmail.com>
2016-10-05 23:19:15 +0200

    setting app/clavas to APPLICATION_BASE_PATH

--- library/OpenSkos2/config.inc.php
+++ library/OpenSkos2/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APPLICATION_BASE_PATH', '/app/ccr');
+define('APPLICATION_BASE_PATH', '/app/clavas');
 define('COMMON_APPLICATION_INI', APPLICATION_BASE_PATH. '/application/configs/application.ini');
 define('ERROR_LOG', APPLICATION_BASE_PATH. '/data/ValidationErrors.txt');
 
