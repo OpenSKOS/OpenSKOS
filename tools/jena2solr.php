db7f78e81143d670626056c0bbc2893f7997950d
cdekok <chris.de.kok@gmail.com>
2016-10-28 14:31:38 +0200

    add option to index single document

--- tools/jena2solr.php
+++ tools/jena2solr.php
@@ -21,13 +21,15 @@
 
 require dirname(__FILE__) . '/autoload.inc.php';
 
-$opts = [
+$options = [
     'env|e=s' => 'The environment to use (defaults to "production")',
+    'uri|u=s' => 'Index single uri e.g -u http://data.beeldengeluid.nl/gtaa/356512',
     'verbose|v' => 'Verbose',
+    'help|h' => 'Show this help',
 ];
 
 try {
-    $OPTS = new Zend_Console_Getopt($opts);
+    $OPTS = new Zend_Console_Getopt($options);
 } catch (Zend_Console_Getopt_Exception $e) {
     fwrite(STDERR, $e->getMessage() . "\n");
     echo str_replace('[ options ]', '[ options ] action', $OPTS->getUsageMessage());
@@ -35,6 +37,10 @@ try {
 }
 
 require dirname(__FILE__) . '/bootstrap.inc.php';
+
+if ($OPTS->getOption('help')) {
+    exit($OPTS->getUsageMessage());
+}
 
 /* @var $diContainer DI\Container */
 $diContainer = Zend_Controller_Front::getInstance()->getDispatcher()->getContainer();
@@ -49,30 +55,28 @@ $logger->pushHandler(new \Monolog\Handler\ErrorLogHandler(
     $logLevel
 ));
 
+$uri = $OPTS->getOption('uri');
+
 /* @var $conceptManager \OpenSkos2\ConceptManager */
 $conceptManager = $diContainer->make('\OpenSkos2\ConceptManager');
 
-$countAllConcepts = "
-    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    prefix owl: <http://www.w3.org/2002/07/owl#>
-
-    SELECT (count(?subject) AS ?count)
-    WHERE {
-      ?subject ?predicate <http://www.w3.org/2004/02/skos/core#Concept>
-    }
-";
-
-$result = $conceptManager->query($countAllConcepts);
-$total = $result->getArrayCopy()[0]->count->getValue();
+$total = getTotal($conceptManager, $uri);
 $rows = 1000;
 
-$fetchConcepts = "
-    DESCRIBE ?subject
-    WHERE {
-      ?subject ?predicate <http://www.w3.org/2004/02/skos/core#Concept>
-    }
-    LIMIT $rows
-";
+if ($uri) {
+    
+    $fetchConcepts = "DESCRIBE <$uri>";
+    
+} else {
+    
+    $fetchConcepts = "
+        DESCRIBE ?subject
+        WHERE {
+          ?subject ?predicate <http://www.w3.org/2004/02/skos/core#Concept>
+        }
+        LIMIT $rows
+    ";
+}
 
 /* @var $solrResourceManager \OpenSkos2\Solr\ResourceManager */
 $solrResourceManager = $diContainer->make('\OpenSkos2\Solr\ResourceManager');
@@ -82,12 +86,13 @@ $offset = 0;
 while ($offset < $total) {
 
     $concepts = $conceptManager->fetchQuery($fetchConcepts . ' OFFSET ' . $offset);
+    
     $offset = $offset + $rows;
-
+    
     foreach ($concepts as $concept) {
 
         $logger->debug($concept->getUri());
-        
+
         try {
             $solrResourceManager->insert($concept);
         } catch (\Exception $exc) {
@@ -102,3 +107,31 @@ $solrResourceManager->commit();
 
 $logger->info('Processed: ' . $total);
 $logger->info("Done!");
+
+/**
+ * Get total amount of concepts
+ * 
+ * @param \OpenSkos2\ConceptManager $conceptManager
+ * @param type $uri
+ * @return int
+ */
+function getTotal(\OpenSkos2\ConceptManager $conceptManager, $uri = null) {
+
+    if ($uri) {
+        return 1;
+    }
+
+    $countAllConcepts = "
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix owl: <http://www.w3.org/2002/07/owl#>
+
+        SELECT (count(?subject) AS ?count)
+        WHERE {
+          ?subject ?predicate <http://www.w3.org/2004/02/skos/core#Concept>
+        }
+    ";
+
+    $result = $conceptManager->query($countAllConcepts);
+    $total = $result->getArrayCopy()[0]->count->getValue();
+    return $total;
+}
