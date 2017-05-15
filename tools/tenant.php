dd486d590a99c30b5cecb90bea71b6624fe2031b
olhsha <olha.chat@gmail.com>
2017-05-15 12:31:27 +0200

    Fixed bug in tenant script (self generate identifiers call was not updated)

--- tools/tenant.php
+++ tools/tenant.php
@@ -130,7 +130,7 @@ function createTenantRdf($code, $name, $epic, $uri, $uuid, $disableSearchInOther
     $tenantResource = new Tenant();
     if ($epic === 'true') {
         try {
-        $uri = $tenantResource->selfGenerateUuidAndUriWhenAbsent($resourceManager, ['type'=>Org::FORMALORG, 'tenantcode' => $code]);
+        $uri = $tenantResource->selfGenerateUri($resourceManager, null, null);
         } catch (Exception $ex) {
            fwrite(STDOUT, "\n Epic failed: " . $ex->getMessage() ." \n");
            fwrite(STDOUT,  "\n I will use the uri and uuid provided by you \n");
