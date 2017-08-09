562f5ad9d7ad1a9cbbdd21bcd41d366d498a47b8
olhsha <olha.chat@gmail.com>
2017-08-09 19:10:27 +0200

    -- experimental commit, trying to locate supervisor for solr

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -14,5 +14,5 @@ cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/solrconfig.xml /opt/solr/serve
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
 cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-solr.sh /home/travis/build/start-solr.sh
 chmod 755 /home/travis/build/start-solr.sh
-mkdir /etc/supervisor/conf.d/
+mkdir -p /etc/supervisor/conf.d
 cp /home/travis/build/OpenSKOS/OpenSKOS/supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
