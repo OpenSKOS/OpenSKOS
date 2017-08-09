f88b471f58525ffcf36c7e7feb6a696ba087e153
olhsha <olha.chat@gmail.com>
2017-08-09 18:58:29 +0200

    -- experimental commit, making a dedicated directory for test settings

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -12,6 +12,6 @@ mkdir -p /opt/solr/server/solr/openskos/conf
 touch /opt/solr/server/solr/openskos/core.properties
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
-cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-solr.sh /start-solr.sh
-chmod 755 /start-solr.sh
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-solr.sh /home/travis/build/start-solr.sh
+chmod 755 /home/travis/build/start-solr.sh
 cp /home/travis/build/OpenSKOS/OpenSKOS/supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
--- integrationtestsettings/start-solr-old.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/solr/example
-exec java -Dsolr.solr.home="./openskos" -Xms1024m -Xmx1024m -jar /opt/solr/example/start.jar &>> /opt/solr/logs/solr-`date '+%Y%m%d'`.log
