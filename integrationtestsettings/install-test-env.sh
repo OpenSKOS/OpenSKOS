ecd79599ecfd7a256d161dc0bb7a76dc55d26baa
olhsha <olha.chat@gmail.com>
2017-08-09 18:51:33 +0200

    -- experimental commit, making a dedicated directory for test settings

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -10,9 +10,8 @@ mkdir /opt/solr
 cp -r /tmp/solr/solr-6.3.0/* /opt/solr 
 mkdir -p /opt/solr/server/solr/openskos/conf 
 touch /opt/solr/server/solr/openskos/core.properties
-ls /home/travis/build/OpenSKOS/OpenSKOS
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
-cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/start-solr.sh /start-solr.sh
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-solr.sh /start-solr.sh
 chmod 755 /start-solr.sh
 cp /home/travis/build/OpenSKOS/OpenSKOS/supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
