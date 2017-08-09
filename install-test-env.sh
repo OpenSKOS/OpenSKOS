15833b385f220137b8e52a30863997a33d2cb6f3
olhsha <olha.chat@gmail.com>
2017-08-09 18:38:55 +0200

    -- experimental commit, trying to locate the files to install solr

--- install-test-env.sh
+++ install-test-env.sh
@@ -10,10 +10,9 @@ mkdir /opt/solr
 cp -r /tmp/solr/solr-6.3.0/* /opt/solr 
 mkdir -p /opt/solr/server/solr/openskos/conf 
 touch /opt/solr/server/solr/openskos/core.properties
-cd /home/travis/build/OpenSKOS/OpenSKOS
-ls
-cp data/solr/config.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
-cp data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
-cp data/solr/start-solr.sh /start-solr.sh
+ls /home/travis/build/OpenSKOS/OpenSKOS
+cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/config.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
+cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
+cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/start-solr.sh /start-solr.sh
 chmod 755 /start-solr.sh
-cp supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
+cp /home/travis/build/OpenSKOS/OpenSKOS/supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
