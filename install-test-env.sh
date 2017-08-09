49525dc511b3df4fae045321942bd12bfd1d6a6e
olhsha <olha.chat@gmail.com>
2017-08-09 17:59:21 +0200

    -- experimental commit, trying to correct the script

--- install-test-env.sh
+++ install-test-env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # install solr:
+ls
 mkdir -p /tmp/solr
 cd /tmp/solr
 wget "https://archive.apache.org/dist/lucene/solr/6.3.0/solr-6.3.0.zip" 
@@ -9,8 +10,8 @@ mkdir /opt/solr
 cp -r /tmp/solr/solr-6.3.0/* /opt/solr 
 mkdir -p /opt/solr/server/solr/openskos/conf 
 touch /opt/solr/server/solr/openskos/core.properties
-cp solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
-cp /data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
-cp /data/solr/start-solr.sh /start-solr.sh
+cp data/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
+cp data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
+cp data/solr/start-solr.sh /start-solr.sh
 chmod 755 /start-solr.sh
 cp supervisord-solr.conf /etc/supervisor/conf.d/supervisord-solr.conf
