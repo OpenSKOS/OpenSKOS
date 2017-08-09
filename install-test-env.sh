d992909c722caaa5c6ad217fac86bc4d68237da5
olhsha <olha.chat@gmail.com>
2017-08-09 18:20:33 +0200

    -- experimental commit, trying to locate the files to install solr

--- install-test-env.sh
+++ install-test-env.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # install solr:
-cd /
-ls
+
 mkdir -p /tmp/solr
 cd /tmp/solr
 wget "https://archive.apache.org/dist/lucene/solr/6.3.0/solr-6.3.0.zip" 
@@ -11,6 +10,7 @@ mkdir /opt/solr
 cp -r /tmp/solr/solr-6.3.0/* /opt/solr 
 mkdir -p /opt/solr/server/solr/openskos/conf 
 touch /opt/solr/server/solr/openskos/core.properties
+cd /home
 cp data/solr/config.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
 cp data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
 cp data/solr/start-solr.sh /start-solr.sh
