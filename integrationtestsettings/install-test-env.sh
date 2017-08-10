f6fdc17056e2b183e7733193dda89361f670a8f0
olhsha <olha.chat@gmail.com>
2017-08-10 14:19:01 +0200

    -- correcting install script

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -25,13 +25,13 @@ chmod +x /opt/apache-jena-fuseki/fuseki-server /opt/apache-jena-fuseki/bin/*
 mkdir -p /opt/apache-jena-fuseki/run 
 cp -r /home/travis/build/OpenSKOS/OpenSKOS/data/fuseki/configuration /opt/apache-jena-fuseki/run/configuration
 mkdir /opt/apache-jena-fuseki/logs
-cp /home/travis/build/OpenSKOS/OpenSKOS/start-fuseki.sh /home/travis/build/start-fuseki.sh
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-fuseki.sh /home/travis/build/start-fuseki.sh
 chmod 755 /home/travis/build/start-fuseki.sh
 
 # mysql:
 
-cp /home/travis/build/OpenSKOS/OpenSKOS/openskos-create.sql /home/travis/build/openskos-create.sql
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/openskos-create.sql /home/travis/build/openskos-create.sql
 
 # initialisation
-cp /home/travis/build/OpenSKOS/OpenSKOS/openskos-init.sh /home/travis/build/openskos-init.sh
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/openskos-init.sh /home/travis/build/openskos-init.sh
 chmod 755 /home/travis/build/openskos-init.sh
