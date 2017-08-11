c93919646e6984d6c2bdb387a72b8dd03deb94bd
olhsha <olha.chat@gmail.com>
2017-08-11 17:02:37 +0200

    -- trying to fix solr (looks like SOLR 5.5 issues)

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -14,7 +14,7 @@ mkdir /opt/solr
 cp -r /tmp/solr/solr-6.3.0/* /opt/solr 
 mkdir -p /opt/solr/server/solr/openskos/conf 
 touch /opt/solr/server/solr/openskos/core.properties
-cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
+cp /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml
 cp /home/travis/build/OpenSKOS/OpenSKOS/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/schema.xml
 chmod 755 /home/travis/build/OpenSKOS/OpenSKOS/integrationtestsettings/start-solr.sh 
 
