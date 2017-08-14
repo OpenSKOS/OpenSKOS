efc382e0c06bef02966fa521e1071c88ed2d2ff6
olhsha <olha.chat@gmail.com>
2017-08-14 12:20:30 +0200

    -- downgrading start-solr.script

--- integrationtestsettings/start-solr.sh
+++ integrationtestsettings/start-solr.sh
@@ -2,4 +2,4 @@
 cd /opt/solr
 find . -name write.lock -exec rm {} \;
 mkdir -p /opt/solr/logs
-exec /opt/solr/bin/solr start -f -force &>> /opt/solr/logs/solr-`date '+%Y%m%d'`.log
+exec /opt/solr/bin/solr start -f &>> /opt/solr/logs/solr-`date '+%Y%m%d'`.log
