5295985c6251fc067bffe5790109fb6b71cc7957
Robin Bron <finwo@pm.me>
2019-10-09 11:42:30 +0200

    Stop grouping ext-install to improve docker cache handling

--- data/docker/scripts/ext-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo " ---> Installing PHP extensions"
-DIR=$(dirname ${0})
-$DIR/ext-docker-install.sh $1 || exit 1
-$DIR/ext-pecl-install.sh $1 || exit 1
-$DIR/ext-custom-install.sh $1 || exit 1
