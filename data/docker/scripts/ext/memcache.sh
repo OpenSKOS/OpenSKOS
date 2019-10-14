f688c009921c4df950a63ed423f00b424047c141
Robin Bron <finwo@pm.me>
2019-10-14 15:57:34 +0200

    Don't rely on docker for pecl memcache install either

--- data/docker/scripts/ext/memcache.sh
+++ data/docker/scripts/ext/memcache.sh
@@ -27,7 +27,7 @@ major=$(echo "${target}" | tr '.' ' ' | awk '{print $1}')
 # PECL memcache for php <7
 if [ "${major}" -lt 7 ]; then
   printf "\n" | pecl install memcache || exit 1
-  docker-php-ext-enable memcache || exit 1
+  echo "extension=memcached.so" >> /usr/local/lib/php.ini
   exit 0
 fi
 
